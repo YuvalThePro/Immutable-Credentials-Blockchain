3892f313ac37512a3a195873f139816f6c78aa5a
YuvalThePro <ifraimovy22@gmail.com>
2025-12-02 20:15:18 +0200

    Added back comments

---
+++ src/main/java/com/immutable/credentials/util/JsonSerializer.java
@@ -0,0 +1,6 @@
+// JsonSerializer.java
+// Utility for JSON serialization/deserialization
+// - blockToJson(): Convert block to JSON
+// - jsonToBlock(): Parse JSON to block
+// - chainToJson(): Serialize entire blockchain
+// - jsonToChain(): Deserialize blockchain
